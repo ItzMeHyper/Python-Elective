27cb7f16bf4b002a0d6e7b6aa04efd20cd178757
ItzMeHyper <vaisakhmanu0005@gmail.com>
2026-05-03 14:17:14 +0530

    Test File

---
+++ OS/OS.py
@@ -0,0 +1,9 @@
+import os
+
+print("OS Name:", os.name)
+
+print("Current Working Directory:", os.getcwd())
+
+print("List of files and directories in the current directory:", os.listdir())
+
+print("Environment Variables:", os.environ)
